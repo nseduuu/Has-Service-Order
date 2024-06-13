0ac24bceaa3b3977f42bbb1bf2140706933298c5
Eduardo Noronha <eduardo.noronhasampaio@gmail.com>
2024-06-13 19:58:37 -0300

    fix(commentservice): method addcommentasync

--- OsDsII.api/Services/Comments/CommentService.cs
+++ OsDsII.api/Services/Comments/CommentService.cs
@@ -1,34 +1,42 @@
 ﻿using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using OsDsII.api.Data;
+using OsDsII.api.Dtos.Comments;
+using OsDsII.api.Exceptions;
 using OsDsII.api.Models;
 using OsDsII.api.Repository.CommentsRepository;
+using OsDsII.api.Repository.ServiceOrderRepository;
 
 namespace OsDsII.api.Services.Comments
 {
-    public class CommentService
+    public class CommentService : ICommentService
     {
         private readonly ICommentsRepository _commentsRepository;
+        private readonly IServiceOrderRepository _serviceOrderRepository;
         private readonly IMapper _mapper;
         private readonly DataContext _context;
 
-        public CommentService(ICommentsRepository commentsRepository, IMapper mapper, DataContext context)
+        public CommentService(ICommentsRepository commentsRepository, IMapper mapper, DataContext context, IServiceOrderRepository serviceOrderRepository)
         {
             _commentsRepository = commentsRepository;
             _mapper = mapper;
             _context = context;
+            _serviceOrderRepository = serviceOrderRepository;
         }
 
-        public async Task AddCommentAsync(int serviceOrderId, Comment comment)
+        public async Task<Comment>AddCommentAsync(int serviceOrderId, CommentDto comment)
         {
-            Comment commentExists = HandleCommentObject(serviceOrderId, comment.Description);
-
-            var os = await _context.ServiceOrders.Include(c => c.Customer).FirstOrDefaultAsync(s => serviceOrderId == s.Id);
+            var commentMapped = _mapper.Map<Comment>(comment);
+            var os = await _serviceOrderRepository.GetByIdAsync(serviceOrderId);
 
             if (os == null)
             {
-                throw new Exception("ServiceOrder not found.");
+                throw new NotFoundException("ServiceOrder not found.");
             }
+
+            Comment commentExists = HandleCommentObject(os.Id, commentMapped.Description);
+
+            return commentExists;
         }
 
         private Comment HandleCommentObject(int id, string description)
